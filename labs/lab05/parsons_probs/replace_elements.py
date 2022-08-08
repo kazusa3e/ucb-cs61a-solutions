cf348db0f1f3891bdacbedfecb1f890f19c5b06d
1eaf <ieaff@outlook.com>
2022-08-08 09:42:41 +0800

    lab 05

---
+++ labs/lab05/parsons_probs/replace_elements.py
@@ -0,0 +1,21 @@
+def replace_elements(source_list, dest_list):
+    """
+    Complete the function replace_elements, a function which takes in source_list
+    and dest_list and mutates the elements of dest_list to be the elements at the
+    corresponding index in source_list.
+    
+    dest_list always has a length greater than or equal to the length of
+    source_list.
+
+    >>> s1 = [1, 2, 3]
+    >>> s2 = [5, 4]
+    >>> replace_elements(s2, s1)
+    >>> s1
+    [5, 4, 3]
+    >>> s3 = [0, 0, 0, 0, 0]
+    >>> replace_elements(s1, s3)
+    >>> s3
+    [5, 4, 3, 0, 0]
+    """
+    for ix in range(len(source_list)):
+        dest_list[ix] = source_list[ix]
